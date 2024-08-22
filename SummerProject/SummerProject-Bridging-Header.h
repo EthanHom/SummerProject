389b2ce3a3fd24a3a4450ae7408e38b2e408fb25
EthanHom <ethanbhom@gmail.com>
2024-08-21 22:30:16 -0700

    Initial commit

---
+++ SummerProject/SummerProject-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  SummerProject-Bridging-Header.h
+//  SummerProject
+//
+//  Created by ethan on 8/21/24.
+//
+
+#import <TesseractOCR/TesseractOCR.h>
+
+#ifndef SummerProject_Bridging_Header_h
+#define SummerProject_Bridging_Header_h
+
+
+#endif /* SummerProject_Bridging_Header_h */
---
+++ SummerProject/SummerProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SummerProjectApp.swift
+//  SummerProject
+//
+//  Created by ethan on 8/21/24.
+//
+
+import SwiftUI
+
+@main
+struct SummerProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
